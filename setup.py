f74f977ffbeee384f02fcda3cf11459c8e411142
Nicole A Montano <n@nicolemon.com>
2017-12-07 14:41:51 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "snote",
+    version = "1.0.0",
+    author = "Nicole A Montano",
+    author_email = "n@nicolemon.com",
+    description = ("A rudimentary CLI to write and organize text"),
+    license = "MIT",
+    keywords = "cli commandline terminal notes python",
+    url = "https://github.com/nicolemon/snote",
+    packages=['snote'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+    extras_require={
+        'test': ['pytest'],
+    },
+    entry_points={
+        'console_scripts': [
+            'snote=snote:main',
+        ],
+    },
+)
